332fb2df8525206b7cee5e83790c72196c78c6b7
Deepika <deepika.bhavnani@arm.com>
2018-06-27 15:08:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Getting started with CPU Stats on uVision Event Recorder
+
+This guide reviews the steps required to get Mbed OS CPU statistics on EventRecorder.
+
+Please install [mbed CLI](https://github.com/ARMmbed/mbed-cli#installing-mbed-cli).
+
+## Import the example application
+
+From the command-line, import the example:
+
+```
+mbed import mbed-os-example-stats-eventrecorder
+cd mbed-os-example-stats-eventrecorder
+```
+
+### Update the linker script for your target device as per `linker_change_example.patch` (K64F)
+Linker script update is required to place the event recorder in uninitialized memory.
+
+### Now export to uVision
+
+Invoke `mbed export`, and specify the name of your platform and uVision
+
+```
+mbed export -m K64F -i uVision
+```
+
+### Enable EventRecorder with the help of below mentioned link
+https://www.keil.com/pack/doc/compiler/EventRecorder/html/er_use.html
+
+### Build and execute program
+When executing this example in the µVision debugger, use the menu command View - Analysis Windows - Event Recorder to open the Event Recorder window. 
---
+++ main.cpp
@@ -0,0 +1,90 @@
+#include "mbed.h"
+#ifdef RTE_Compiler_EventRecorder
+#include "EventRecorder.h"
+#endif
+
+// The example is to demonstrate CPU stats on Event Recorder
+// CPU load is increased suddenly for single sample time to add an anomaly 
+ 
+#include "rtx_evr.h"
+
+#if !defined(MBED_CPU_STATS_ENABLED) || !defined(DEVICE_LPTICKER) || !defined(DEVICE_SLEEP)
+#error [NOT_SUPPORTED] test not supported
+#endif
+
+#define MbedDevStats             (0x81U)
+#define MbedCpuStats              EventID(EventLevelAPI,    MbedDevStats, 0x01U)
+#define MbedCpuLoad               EventID(EventLevelAPI,    MbedDevStats, 0x02U)
+
+DigitalOut led1(LED1);
+
+#define MAX_THREAD_STACK        384
+#define SAMPLE_TIME             1000
+#define INTERMEDIATE_TIME       5000
+
+uint64_t prev_idle_time = 0;
+int32_t wait_time = 5000;
+
+void busy_thread()
+{
+    volatile uint64_t i = ~0;
+
+    while(i--) {
+        led1 = !led1;
+        wait_us(wait_time);
+    }
+}
+
+void cpu_stats_queue()
+{
+    mbed_stats_cpu_t stats;
+    mbed_stats_cpu_get(&stats);
+
+   (void)EventRecord2(MbedCpuStats, (uint32_t)stats.uptime, (uint32_t)(stats.idle_time - prev_idle_time));
+    prev_idle_time = stats.idle_time;
+}
+
+void cpu_load_queue()
+{
+    mbed_stats_cpu_t stats;
+    mbed_stats_cpu_get(&stats);
+
+    uint64_t diff = (stats.idle_time - prev_idle_time);
+    uint8_t idle = (diff * 100) / (SAMPLE_TIME*1000);    // usec;
+    uint8_t usage = 100 - ((diff * 100) / (SAMPLE_TIME*1000));    // usec;;
+    prev_idle_time = stats.idle_time;
+
+   (void)EventRecord2(MbedCpuLoad, idle, usage);
+}
+
+int main()
+{
+    // Request the shared queue
+    EventQueue *stats_queue = mbed_event_queue();
+    Thread *thread;
+    int id;
+
+    EventRecorderInitialize(EventRecordAPI, 1);  // initialize and start Event Recorder
+    EventRecorderEnable (EventRecordAPI, MbedDevStats, MbedDevStats);
+    EventRecorderStart();
+    
+    id = stats_queue->call_every(SAMPLE_TIME, cpu_stats_queue);
+    
+    for (int i = 0; i < 2; i++) {
+        thread = new Thread(osPriorityNormal, MAX_THREAD_STACK);
+        thread->start(busy_thread);
+
+        // Suddenly increase the system load
+        Thread::wait(INTERMEDIATE_TIME);
+        wait_time -= 4500;
+        Thread::wait(SAMPLE_TIME);
+        wait_time += 4500;
+        Thread::wait(INTERMEDIATE_TIME);
+        
+        stats_queue->cancel(id);
+        id = stats_queue->call_every(SAMPLE_TIME, cpu_load_queue);
+    }
+    thread->terminate();
+    stats_queue->cancel(id);
+    return 0;
+}
